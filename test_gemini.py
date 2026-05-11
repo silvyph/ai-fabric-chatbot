8e97b59a49e63e11954afe38a768747aa638393b
Silvy Putri Hanafi <95921766+silvyph@users.noreply.github.com>
2026-05-11 21:55:40 +0700

    Update test_gemini.py

--- test_gemini.py
+++ test_gemini.py
@@ -1,6 +1,6 @@
 from google import genai
 
-client = genai.Client(api_key="AIzaSyCFmUsicq2O-R_gn35Ycgek3Ql7A-D_oIU")
+client = genai.Client(api_key="YOUR_API_KEY")
 
 response = client.models.generate_content(
     model="gemini-2.5-flash",
